b759534c002bfb749ac5d9acdb82ab23de1860fc
BushraAlabsi <boushra-93@live.com>
2018-12-31 03:42:03 +0200

    renders a requests page with mock data

--- feature_req/routes.py
+++ feature_req/routes.py
@@ -1,7 +1,24 @@
-
+from flask import render_template
 from feature_req import app
-
+requests1 = [
+    {
+        'title': 'Corey Schafereeee',
+        'description': 'Blog Post 1',
+        'client': 'First post content',
+        'target_date': 'April 20, 2018',
+        'area': 'pa1',
+        'client_priority':'1'
+    },
+    {
+        'title': 'Jane Doe',
+        'description': 'Blog Post 2',
+        'client': 'Second post content',
+        'target_date': 'April 21, 2018',
+        'area': 'pa2',
+        'client_priority':'2'
+    }
+]
 
 @app.route("/")
-def hello():
-    return "hello"
+def requests():
+    return render_template("requests.html", title="home",requests= requests1)
