abfaacb2fb0740a171b6fb4eec642550fcccd9a1
BushraAlabsi <boushra-93@live.com>
2019-01-04 02:10:28 +0200

    adding config properties to environment variables

--- feature_req/__init__.py
+++ feature_req/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '26491f801b9edd98a9035e08f95a93d8'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///request.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
